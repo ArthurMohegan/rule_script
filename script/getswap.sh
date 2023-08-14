c6c48fae87e416678b141e86fa90a90b5d930446
Arthur Mohegan <3363385+ArthurMohegan@users.noreply.github.com>
2023-08-14 15:14:35 +0800

    update swap

--- script/getswap.sh
+++ script/getswap.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 echo -e "pid\tswap\tproc_name"
-for pid in $(ls -l /proc | grep ^d | awk '{ print $9 }'| grep  -v [^0-9]); do
-    # 判断改进程是否占用了swap
-    grep -q "Swap" /proc/$pid/smaps 2>/dev/null
-    if [ $? -eq 0 ];then
-    # 如果占用了swap
-        swap=$(grep Swap /proc/$pid/smaps | gawk '{ sum+=$2;} END { print sum }')
-        proc_name=$(ps aux | grep -w "$pid" | grep -v grep  | awk '{ for(i=11;i<=NF;i++){ printf("%s ",$i); }}')
-        if [ $swap -gt 0 ];then
-        # 输出swap信息
+for pid in $(ls /proc | grep -E '^[0-9]+$'); do
+    if [[ -f /proc/$pid/smaps ]]; then
+        swap=$(awk '/Swap:/ { sum += $2 } END { print sum }' /proc/$pid/smaps)
+        if [[ $swap -gt 0 ]]; then
+            proc_name=$(ps -p $pid -o cmd=)
             echo -e "$pid\t${swap} KB\t$proc_name"
         fi
     fi
-    done | sort -k2 -n
+done | sort -k2 -n
